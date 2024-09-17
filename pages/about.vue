4be86335fa210c1e8dd72cff1b714104434ccfd1
Arash Abdous <arash.abdous@gmail.com>
2024-09-17 20:00:29 +0330

    init

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <h1 class="text-4xl font-bold text-blue-600">
-      Welcome to My Nuxt.js Project with Tailwind CSS!
+      about
     </h1>
-    <nuxt-link to="/about">ABOUT</nuxt-link>
   </div>
 </template>
